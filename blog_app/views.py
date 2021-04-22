8efc515a7f68a2b722a83af11141ecf8ec0cb473
Abhilash BK <bkabhilash0@gmail.com>
2021-04-22 22:01:13 +0530

    Done with form handling but not the senting Mail

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,get_object_or_404
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from django.views.generic import ListView
+from django.core.mail import send_mail
 from .models import Post
+from .forms import EmailPostForm
 
 # Create your views here.
 def post_list(request):
@@ -15,6 +18,33 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     return render(request,'blog/post/list.html',{'posts': posts,'page':page})
 
+class PostListView(ListView):
+    queryset = Post.published.all();
+    context_object_name = 'posts';
+    paginate_by = 2;
+    template_name = 'blog/post/list.html';
+
 def post_detail(request,year,month,day,post):
     post = get_object_or_404(Post,slug=post,status='published',publish__year=year,publish__month=month,publish__day=day);
     return render(request,'blog/post/detail.html',{'post':post})
+
+def post_share(request,post_id):
+    post = get_object_or_404(Post,id=post_id,status='published')
+    sent = False
+    if(request.method == 'POST'):
+        # Form was Submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed Validation
+            cd = form.cleaned_data
+            sent = True
+            # sent Email
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+            f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+            f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+    else:
+        form = EmailPostForm()
+    return render(request,'blog/post/share.html',{'form':form,'post':post,'sent':sent})
