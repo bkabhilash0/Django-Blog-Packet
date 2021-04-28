6fd44591b6ba96ff086e3a8cf5a0fb8f8b1b2ecd
Abhilash BK <bkabhilash0@gmail.com>
2021-04-28 21:24:03 +0530

    Done with Tagging Function using Taggit

--- blog_app/views.py
+++ blog_app/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from django.db.models import Count
+from taggit.models import Tag
 from .models import Post,Comment
 from .forms import EmailPostForm,CommentForm
 
 
 # List All Posts with Normal Method.
-def post_list(request):
+def post_list(request,tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag,slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
     paginator = Paginator(object_list, 2)
     page = request.GET.get('page')
     try:
@@ -17,7 +23,7 @@ def post_list(request):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'posts': posts, 'page': page})
+    return render(request, 'blog/post/list.html', {'posts': posts, 'page': page,'tag':tag})
 
 
 # List all Posts using Class Based Views
@@ -49,7 +55,12 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
-    return render(request, 'blog/post/detail.html', {'post': post,'comments':comments,'new_comment':new_comment,'comment_form':comment_form})
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+    print(similar_posts)
+    return render(request, 'blog/post/detail.html', {'post': post,'comments':comments,'new_comment':new_comment,
+    'comment_form':comment_form,'similar_posts':similar_posts})
 
 
 # Render a Form to share the Post via Email.
